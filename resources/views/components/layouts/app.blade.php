2b4c909b101e9c5ef588be3440aa19df743fce18
Judel Fintch <71493804+judelFintch@users.noreply.github.com>
2025-07-13 12:50:33 +0200

    fix Livewire root element

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <title>{{ $title ?? 'New Custom Agency' }}</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/home-page.js'])
     @livewireStyles
 </head>
 <body class="font-sans antialiased">
